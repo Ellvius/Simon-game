3ee72f20640727f6531c87956d3f81984a36f5a9
Lishin <lishinvs6541@gmail.com>
2024-07-29 17:29:40 +0530

    add button to start and restart in place of keypress

--- script.js
+++ script.js
@@ -1,99 +1,90 @@
-
 var userClickedPattern = [];
 var gamePattern = [];
 var buttonColors = ["red", "blue", "green", "yellow"];
 var level = 0;
 var started = false;
 
-// Detecting keyboard press and starting the game
-$("body").keypress(function(){
-  if(!started){
-    $("h1").html("level " + level);
+// Function to start the game
+function startGame() {
+  if (!started) {
+    $("h1").html("Level " + level);
     nextSequence();
     started = true;
   }
-});
+}
+
+//starting the game
+$(".start").click(startGame);
 
 // User generated button clicks
-$(".btn").click(function (){
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
 
-    var userChosenColor = $(this).attr("id");
+  // Storing the user generated sequence
+  userClickedPattern.push(userChosenColor);
 
-    // Storing the user generated sequence
-    userClickedPattern.push(userChosenColor);
+  // Adding sound and animation to button click
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
 
-    // Adding sound and animation to button click
-    animatePress(userChosenColor);
-    playSound(userChosenColor);
-
-    checkAnswer(userClickedPattern.length -1);
-
+  checkAnswer(userClickedPattern.length - 1);
 });
-
 
 // System generated pattern
 function nextSequence() {
+  userClickedPattern = [];
 
-    userClickedPattern = [];
+  level++;
+  $("h1").html("Level " + level);
 
-    level++;
-    $("h1").html("level " + level);
+  // Generate random pattern
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
 
-    // Generate random pattern
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColor = buttonColors[randomNumber];
+  // Storing the randomly generated sequence
+  gamePattern.push(randomChosenColor);
 
-    // Storing the randomly generated sequence
-    gamePattern.push(randomChosenColor);
-
-    // Adding sound and animation
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
+  // Adding sound and animation
+  $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
 }
 
-
-
-
 // Function to play sound
-function playSound(name){
+function playSound(name) {
   var audio = new Audio("sounds/" + name + ".mp3");
   audio.play();
 }
 
-
 // Adding animation
-function animatePress(currentColor){
+function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
-  setTimeout( function(){
-    $("#" + currentColor).removeClass("pressed")
-  }, 100 );
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
 }
 
-// checking the answers
-function checkAnswer(currentLevel){
-  if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
-    if(userClickedPattern.length === gamePattern.length){
-      
-      setTimeout(function(){
-        nextSequence()
+// Checking the answers
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
       }, 1500);
     }
-
-  }
-  else {
+  } else {
     playSound("wrong");
-    $("h1").html("Game over <br> Press A Key to Restart");
+    $("h1").html('<div class="start">Restart</div>');
     $("body").addClass("game-over");
-    setTimeout(function(){
+    setTimeout(function () {
       $("body").removeClass("game-over");
     }, 200);
     startOver();
+    $(".start").click(startGame); // Re-attach event listener to the restart button
   }
 }
 
-
 // Restarting the game
-function startOver(){
+function startOver() {
   level = 0;
   gamePattern = [];
   started = false;
